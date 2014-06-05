6cc7cc64d16ee895512b5453130aed313d2e8fcc
hemachandrareddy.t@gmail.com <hemachandrareddy.t@gmail.com@2e3b270c-683b-7ece-8240-e5b09faff832>
2014-06-05 13:00:28 +0000

    Added alert and progress dialog for login

--- trunk/src/com/droidrtc/util/Constants.java
+++ trunk/src/com/droidrtc/util/Constants.java
@@ -4,6 +4,6 @@ public interface Constants {
 
 	String SERVER_HOST = "ec2-54-187-247-71.us-west-2.compute.amazonaws.com";
 	int SERVER_PORT = 5222;
-	/*String USERNAME = "chandu";
-	String PASSWORD = "chandu";*/
+	
+	
 }
